21d3b39b166de3c6744f75768260dc86ca61542b
Shane Rogers <shane@repllabs.com>
2023-01-03 10:24:42 -0800

    setup basic html plugin

---
+++ src/code.ts
@@ -0,0 +1,58 @@
+const insertText = async (value: string) => {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
+  const textNode = figma.createText()
+  
+  textNode.characters = value
+  figma.currentPage.appendChild(textNode);
+
+  figma.viewport.scrollAndZoomIntoView([textNode]);
+}
+
+const insertPieChart = async (value: number[]) => {
+  const width = 100
+  const height = 100
+
+  const frame = figma.createFrame()
+  figma.currentPage.appendChild(frame)
+  frame.resizeWithoutConstraints(width, height)
+
+  const segmentsTotal = value.reduce((memo, segment) => memo + segment, 0)
+  
+  let start = 0;
+
+  for (const num of value) {
+    const c = Math.sqrt(start / segmentsTotal)
+    const ellipse = figma.createEllipse()
+    frame.appendChild(ellipse)
+    ellipse.resizeWithoutConstraints(width, height)
+    ellipse.fills = [{ type: 'SOLID', color: {r: c, g: c, b: c} }]
+    ellipse.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
+    ellipse.arcData = {
+      startingAngle: (start / segmentsTotal - 0.25) * 2 * Math.PI,
+      endingAngle: ((start + num) / segmentsTotal - 0.25) * 2 * Math.PI,
+      innerRadius: 0,
+    }
+    start += num
+  }
+
+  figma.viewport.scrollAndZoomIntoView([frame]);
+}
+
+figma.showUI(__html__)
+
+interface IMessageInput {
+  type: string,
+  value: string | number[]
+}
+
+figma.ui.onmessage = async ({ type, value }: IMessageInput) => {
+  if (type === 'text') {
+    await insertText(value as string)
+  } else if (type === 'pie-chart') {
+    await insertPieChart(value as number[])
+  } else {
+    figma.notify(`Unsupported type: ${type}`, {error: true})
+  }
+
+  figma.closePlugin()
+}
